fe68e9f4de635132428e4634ce895d1dc1ebb0d1
GabrielGSD <gabriel.daniel@gec.inatel.br>
2025-02-25 13:47:01 -0300

    feat: add Kafka producer initialization

--- fullcycle/go-kafka/cmd/producer/main.go
+++ fullcycle/go-kafka/cmd/producer/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func NewKafkaProducer() *kafka.Producer {
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "go-kafka-kafka-1:9092",
+	}
+	p, err := kafka.NewProducer(configMap)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	return p
 }
